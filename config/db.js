9341c0f1f6ca49552d85e9961f4b9601cb8403e6
udai sai <karmuriudaisai002@gmail.com>
2025-04-23 13:06:33 +0530

    first commit

---
+++ config/db.js
@@ -0,0 +1,16 @@
+// udai123
+// mongodb+srv://karumuriudaisai002:<db_password>@cluster0.ejw0cit.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+const mongoose = require("mongoose")
+
+const connectDb = async ()=>{
+    try{
+        const conn = await mongoose.connect(process.env.MONGDB_URL)
+
+        console.log(`Mongodb connected:${conn.connection.host}`)
+    }catch(e){
+        console.log(`error in connecting mongdb ${e}`)
+        process.exit(1)
+    }
+}
+
+module.exports = connectDb
---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express")
+const cors = require("cors")
+const connectDb = require("./config/db")
+const session = require("express-session")
+require("dotenv").config() 
+const port = process.env.PORT || 3001
+
+connectDb()
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.use(session({
+    secret: 'medicine on minute',      // 🔑 used to sign the cookie
+    resave: false,                        // 💤 don't save session if unmodified
+    saveUninitialized: false,            // 🐣 don't create session until something is stored
+    cookie: {
+      maxAge: 1000 * 60 * 60,            // 1 hour
+      secure: false,                     // ⚠️ true if using HTTPS
+      httpOnly: true                     // 🔐 can't access cookie via JS
+    }
+  }));
+
+
+
+app.use("/" , (req , res)=>{
+    res.send("welcome to mom pharmacy app ")
+})
+
+app.listen(port , ()=>{
+    console.log(`app is listening at http://localhost:${port}`)
+})
